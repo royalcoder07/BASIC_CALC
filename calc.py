3b42cba2854f367429777213a0d41fea6bfa8971
anurag <123anuraggupta@gmail.com>
2026-07-07 14:55:35 +0530

    logic of calculator

---
+++ calc.py
@@ -0,0 +1,12 @@
+a=int(input("ENTER FIRST NUMBER :"))
+b=int(input("ENTER SECOND NUMBER :"))
+c=input("ENTER OPERATION YOU WANT TO PERFORM (ie-> +,-,*,%,/):")
+
+if(c=='+'): print(a+b)
+if(c=='-'): print(a-b)
+if(c=='*'): print(a*b)
+if(c=='%'): print(a%b)
+if(c=='/'): print(a%b)
+
+
+
